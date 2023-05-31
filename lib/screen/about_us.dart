095cefc7bb4282eba122f07c76935c3314eae546
ahmed.raza <ahmed.raza@securetech-consultancy.com>
2023-05-31 15:36:34 +0500

    Drawer added again

--- lib/screen/about_us.dart
+++ lib/screen/about_us.dart
@@ -1,37 +1,4 @@
-<<<<<<< HEAD
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
-class AboutUs extends StatelessWidget {
-  const AboutUs({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
-      appBar: AppBar(
-        title: const Text('About Us'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          ClipRRect(
-              borderRadius: BorderRadius.circular(30.0),
-              child: Image.asset('assets/gomaal_shareef.jpg',height: 140,width: 200,)),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Icon(Icons.copyright),
-              SizedBox(width: 20),
-              Text('Shajra Shareef Copyrights 2023'),
-            ],
-          ),
-        ],
-      ),
-    ));
-  }
-}
-=======
 import 'package:flutter/material.dart';
 
 import '../configurations/size_config.dart';
@@ -107,5 +74,3 @@ class AboutUsScreen extends StatelessWidget {
     );
   }
 }
-
->>>>>>> 477c016 (Drawer added)
